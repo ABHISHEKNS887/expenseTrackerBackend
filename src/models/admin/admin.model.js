4dd14ef65bce0d8f06fb8e8e874124f031b831b8
Abhishek N S <abhishek.ns@shopalyst.com>
2024-06-12 21:17:10 +0530

    created admin model

---
+++ src/models/admin/admin.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const adminSchema = new Schema({
+    emial: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    refreshToken: {
+        type: String
+    }
+}, {timestamps: true})
+
+adminSchema.pre("save", async function (next) {
+    if (!this.modified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+adminSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+adminSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            userName: this.userName,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+adminSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const Admin = new mongoose.model('Admin', adminSchema)
